f970366988778186483d2319ba7c956602e92efc
Rexqwer911 <rexqwer911@gmail.com>
2023-05-31 10:14:25 +0300

    Версия 2.2.1 Фикс для записи логов

--- src/main/java/com/rexqwer/telegrambotassistant/service/MessageProcessingService.java
+++ src/main/java/com/rexqwer/telegrambotassistant/service/MessageProcessingService.java
@@ -4,12 +4,15 @@ import com.rexqwer.telegrambotassistant.config.ApplicationProperties;
 import com.rexqwer.telegrambotassistant.domain.Message;
 import com.rexqwer.telegrambotassistant.domain.User;
 import com.rexqwer.telegrambotassistant.domain.reference.MessageType;
+import com.rexqwer.telegrambotassistant.enums.MessageTypeEnum;
+import com.rexqwer.telegrambotassistant.event.SendMessageEvent;
 import com.rexqwer.telegrambotassistant.service.branch.MessageBranchSelector;
 import com.rexqwer.telegrambotassistant.service.message.MessageRequestService;
 import com.rexqwer.telegrambotassistant.service.message.MessageService;
 import com.rexqwer.telegrambotassistant.service.message.MessageTypeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -26,6 +29,7 @@ public class MessageProcessingService {
     private final VoiceMessageService voiceMessageService;
     private final MessageBranchSelector messageBranchSelector;
     private final ApplicationProperties applicationProperties;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public void processMessage(Update update) {
         if (update.getMessage() != null && update.getMessage().getFrom() != null) {
@@ -44,7 +48,11 @@ public class MessageProcessingService {
                 if (update.getMessage().isCommand()) {
                     commandService.processCommand(message);
                 } else {
-                    messageBranchSelector.process(message);
+                    if (messageType.getCode().equals(MessageTypeEnum.LOG.getCode())) {
+                        applicationEventPublisher.publishEvent(new SendMessageEvent("Лог записан! :)", message.getChatId()));
+                    } else {
+                        messageBranchSelector.process(message);
+                    }
                 }
             } else {
                 if (update.getMessage().hasVoice()) {
